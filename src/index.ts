87ead242c5c28c6b34383caa2a0861d879e2cfe0
martinavagyan <martinavagyan@gmail.com>
2025-03-04 23:35:26 +0400

    feat: upgrade SDK to support flat curve

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { sellIx } from './sellIx';
 import { buyIx } from './buyIx';
 import { createMint } from './create-mint';
+import { buyFlatCurveTokenIx } from './buyFlatCurveTokenIx';
+import { sellFlatCurveTokenIx } from './sellFlatCurveTokenIx';
 
 const main = async (): Promise<void> => {
   await buyIx();
   await sellIx();
   await createMint();
+
+  // Flat curve examples
+  await buyFlatCurveTokenIx();
+  await sellFlatCurveTokenIx();
 };
 
 main().catch(console.error);
